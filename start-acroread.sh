4776e1ee7468ddbc4cc3701c5f46282e5e705268
Chris Daish <chrisdaish@gmail.com>
2015-04-13 13:07:12 +0100

    Check direcory and permissions at runtime.

--- start-acroread.sh
+++ start-acroread.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 groupmod -g $gid acroread
 usermod -u $uid -g $gid acroread
+
+if [ -d /home/acroread/.adobe ]; then
+  chown -R acroread:acroread /home/acroread/.adobe
+fi
+
 exec su -ls "/bin/bash" -c "mkdir -p /home/acroread/.local/share; /usr/bin/acroread $ARGS $FILE" acroread
